a3504350bd33bb105a5b620809e25b81323a177e
Roviil <qowjdgns0106@naver.com>
2022-11-02 19:20:17 +0900

    version1.0

--- main.js
+++ main.js
@@ -4,35 +4,41 @@ var url = require('url');
 const path = require('path');
 var qs = require('querystring')
 
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-</body>
-</html>
-  `;
-}
-
-function templateList(filelist) {
-  var list = '<ol>';
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
+var template = {
+  html:function(title, list, body, control) {
+    return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${control}
+        ${body}
+      </body>
+    </html>
+    `;
+  },
+  list:function(filelist) {
+    var list = '<ol>';
+    var i = 0;
+    while (i < filelist.length) {
+      list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list + '</ol>';
+    return list
+  
   }
-  list = list + '</ol>';
-  return list
 
 }
+
+
+
+
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -177,12 +183,12 @@ var app = http.createServer(function (request, response) {
     request.on('end', function(){
       var post = qs.parse(body);
       var id = post.id;
-      fs.unlinkSync(`data/${id}`, function(err){
+      fs.unlink(`data/${id}`, function(err){
         response.writeHead(302, {Location: `/`});
         response.end();
       })
       
-      });
+    });
     
   }
   else {
